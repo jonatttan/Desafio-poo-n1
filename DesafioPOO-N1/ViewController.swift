5d40bccf55aa008a89f9a9243100ed1d6a0161f8
Jonattan <jonattan.sousa@gmail.com>
2021-05-13 14:28:09 -0300

    Pequenas modificações em relação à ViewController.

--- DesafioPOO-N1/ViewController.swift
+++ DesafioPOO-N1/ViewController.swift
@@ -15,13 +15,17 @@ class ViewController: UIViewController {
     }
     func bora() {
         var listaFuncionarios: [Funcionario] = []
-        let listaCriaObjetos = [["GER","Silvonei Razuke",4500.0],["GER","Viviane Cabral",8000.0],["GER","Matias Dourado",6000.0],["TEST","Vinicius Garcia",3000.0],["DBA","Lilian Vieira",2000.0],["DEV","Ferndo Oliveira",3500.0],["DEV","Jorge Matias",2500.0],["DEV","Claudinei Santos",5000.0],["DEV","Silvana Amorim",1500.0],["DEV","Geovana Silva",2500.0],["DEV","Bianca Torres",3800.0],["DBA","Luana Silva",1500.0],["DBA","Bianca Torres",3800.0]]
+        let listaCriaObjetos = [["GER","Silvonei Razuke",4500.0],["GER","Viviane Cabral",8000.0],["GER","Matias Dourado",6000.0]]// ,["TEST","Vinicius Garcia",3000.0],["DBA","Lilian Vieira",2000.0],["DEV","Ferndo Oliveira",3500.0],["DEV","Jorge Matias",2500.0],["DEV","Claudinei Santos",5000.0],["DEV","Silvana Amorim",1500.0],["DEV","Geovana Silva",2500.0],["DEV","Bianca Torres",3800.0],["DBA","Luana Silva",1500.0],["DBA","Bianca Torres",3800.0]]
         
-        print("Total de funcionarios: \(listaFuncionarios.count)")
-        for registro in listaCriaObjetos {
-            guard let cargo = registro[0] as? String else { return }
-            guard let nome = registro[1] as? String else { return }
-            guard let salario = registro[2] as? Double else { return }
+        listaFuncionarios = cadastraFuncionarios(listaCriaObjetos)
+        exibeFuncionarios(listaFuncionarios)
+    }
+    func cadastraFuncionarios(_ lista: [[Any]]) -> [Funcionario] {
+        var listaFuncionarios: [Funcionario] = []
+        for registro in lista {
+            guard let cargo = registro[0] as? String else { return listaFuncionarios }
+            guard let nome = registro[1] as? String else { return listaFuncionarios }
+            guard let salario = registro[2] as? Double else { return listaFuncionarios }
             if cargo == "DEV" {
                 listaFuncionarios.append(Desenvolvedor(nome, salario))
             }
@@ -35,10 +39,12 @@ class ViewController: UIViewController {
                 listaFuncionarios.append(Gerente(nome, salario))
             }
         }
-        // FUNCIONARIOS
-        for funcionario in listaFuncionarios {
+        return listaFuncionarios
+    }
+    func exibeFuncionarios(_ lista: [Funcionario]) {
+        for funcionario in lista {
             funcionario.printaResumo()
         }
-        print("Total de funcionarios: \(listaFuncionarios.count)")
+        print("Total de funcionarios: \(lista.count)")
     }
 }
